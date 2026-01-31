5f7fed4afdb1722587c9819bdc6a9c44235e0a39
DIYADINACHANDRAN <diyadinachandran62@gmail.com>
2026-01-31 10:31:57 +0530

    Add files via upload

---
+++ final.java
@@ -0,0 +1,8 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("Diya");
+        System.out.println("Artificial Intelligence and Data Science Student");
+        System.out.println("Learning Python, C and Data Structures");
+        System.out.println("Skills: Python, Data Structures, GitHub");
+    }
+}
